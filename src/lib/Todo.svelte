0d8cb8f72ef62202bd2aeac42e4a2c08e3be4116
rizalshmbg <sihombingrizal18@gmail.com>
2025-06-15 21:49:45 +0700

    $each block using id as key

--- src/lib/Todo.svelte
+++ src/lib/Todo.svelte
@@ -1,9 +1,23 @@
 <script>
 	const { id, name, done } = $props();
+
+	const emojis = {
+		"1": "👍",
+		"2": "👎",
+		"3": "🤷",
+		"4": "🤔",
+		"5": "🎉",
+		"6": "🚀",
+	}
+
+	const emoji = emojis[id]
+	console.log(emoji)
 </script>
 
+{emoji}
+
 {#if done}
-	<strike>{id} - {name}</strike>
+<strike>{id} - {name}</strike>
 {:else}
-	{id} - {name}
+{id} - {name}
 {/if}
--- src/lib/TodoApp.svelte
+++ src/lib/TodoApp.svelte
@@ -1,18 +1,24 @@
 <script>
 	import Todo from './Todo.svelte';
 
-	const data = [
+	const data = $state([
 		{ id: 1, name: 'Belajar HTML', done: true },
 		{ id: 2, name: 'Belajar CSS', done: true },
 		{ id: 3, name: 'Belajar React', done: true },
 		{ id: 4, name: 'Belajar Next', done: false },
 		{ id: 5, name: 'Belajar Svelte', done: true },
 		{ id: 6, name: 'Belajar Nuxt', done: false },
-	];
+	]);
+
+	function remove() {
+		data.shift();
+	}
 </script>
 
+<button onclick={remove}>Remove</button>
+
 <ul>
-	{#each data as todo}
+	{#each data as todo (todo.id)}
 		<li>
 			<Todo {...todo} />
 		</li>
