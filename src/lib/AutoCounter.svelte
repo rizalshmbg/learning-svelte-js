336b090e7e86e0aca07ac9278c034bc0293b9149
rizalshmbg <sihombingrizal18@gmail.com>
2025-06-15 20:03:07 +0700

    rune $effect

---
+++ src/lib/AutoCounter.svelte
@@ -0,0 +1,17 @@
+<script>
+	let counter = $state(0);
+	let interval = $state(1000)
+
+	function increment() {
+		interval += 1000
+	}
+
+	$effect(() => {
+		const id = setInterval(() => {
+			counter += 1
+		}, interval)
+	})
+</script>
+
+<h1>Counter : {counter}</h1>
+<button onclick={increment}>Increment Interval : {interval} ms</button>
